eafbb54a386ef989420bcdf53177d5035230ebb0
Corbin Robb <corbinrobb@gmail.com>
2020-06-08 14:06:50 -0600

    Fix response statuses and attempt stretch

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -4,20 +4,15 @@ const db = require('../data/dbConfig');
 
 router.get('/', async (req, res) => {
   try {
-    const accounts = await db('accounts');
+    const accounts = await db('accounts').limit(req.query.limit || 20).orderBy(req.query.sortby || 'id');
     res.status(200).json(accounts);
   } catch(err) {
-    res.status(500).json({ error: err});
+    res.status(500).json({ error: "Could not retrieve accounts from database"});
   }
 });
 
 router.get('/:id', validateAccountId, async (req, res) => {
-  try {
-    const account = await db('accounts').where({ id: req.account });
-    res.status(200).json(account);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
+  res.status(200).json(req.account);
 });
 
 router.post('/', validateAccount, async (req, res) => {
@@ -25,7 +20,7 @@ router.post('/', validateAccount, async (req, res) => {
     const account = await db('accounts').insert(req.body);
     res.status(200).json(account);
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: "Could not add post to database" });
   }
 });
 
@@ -34,24 +29,28 @@ router.put('/:id', validateAccountId, validateAccount, async (req, res) => {
     const updatedAccount = await db('accounts').where({ id: req.params.id}).update(req.body);
     res.status(200).json(updatedAccount);
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: "Could not change post in database" });
   }
 });
 
 router.delete('/:id', validateAccountId, async (req, res) => {
   try {
-    const deleted =  await db('accounts').where({ id: req.account }).del();
+    const deleted =  await db('accounts').where({ id: req.params.id }).del();
     res.status(200).json(deleted);
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: "Could not delete account in database" });
   }
 });
 
 async function validateAccountId(req, res, next) {
-  const account = await db('accounts').where({ id: req.params.id });
-  if (account.length === 0) return res.status(404).json({ error: "Invalid Account ID" });
-  req.account = req.params.id;
-  next();
+  try {
+    const account = await db('accounts').where({ id: req.params.id });
+    if (account.length === 0) return res.status(404).json({ error: "Invalid Account ID" });
+    req.account = account;
+    next();
+  } catch {
+    res.status(500).json({ error: "Could not retrieve account from database" });
+  }
 }
 
 function validateAccount(req, res, next) {
